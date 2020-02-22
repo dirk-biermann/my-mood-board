2bd8480e8bcd48a6b711b10067e2d50527945c20
Biermann, Dirk <biermannd@mediamarktsaturn.com>
2020-02-22 17:01:08 +0100

    4 build

---
+++ client/build/precache-manifest.66da86922bc1c60389a892504160e8a7.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d0233b6ecebe4810b276988e1f067b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff42d7a94aae4d1a9480",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ed622722fec84153a5cc",
+    "url": "/static/css/main.8f71d5ec.chunk.css"
+  },
+  {
+    "revision": "ff42d7a94aae4d1a9480",
+    "url": "/static/js/2.fa86304d.chunk.js"
+  },
+  {
+    "revision": "ed622722fec84153a5cc",
+    "url": "/static/js/main.00ba4304.chunk.js"
+  },
+  {
+    "revision": "18ceee0904d39392a2a3",
+    "url": "/static/js/runtime-main.8095ec4f.js"
+  },
+  {
+    "revision": "42e7391741923502fe09753d44ba8a1b",
+    "url": "/static/media/cancel.42e73917.svg"
+  },
+  {
+    "revision": "2365db527927fb1a2e4e8fe85db30a2a",
+    "url": "/static/media/checked.2365db52.svg"
+  },
+  {
+    "revision": "e97cbcdd142187a672e0bb936322a042",
+    "url": "/static/media/delete.e97cbcdd.svg"
+  },
+  {
+    "revision": "5704d65333827ac0a69e3d60dcde6c2f",
+    "url": "/static/media/diff.5704d653.svg"
+  },
+  {
+    "revision": "a4d1d8daf577d04803388d06cab9aaaf",
+    "url": "/static/media/doc.a4d1d8da.svg"
+  },
+  {
+    "revision": "9ff66543846749acd058d928da92a286",
+    "url": "/static/media/down.9ff66543.svg"
+  },
+  {
+    "revision": "aaf6059a7610c0c34b4be243c473333f",
+    "url": "/static/media/edit.aaf6059a.svg"
+  },
+  {
+    "revision": "b5ae6b7f04cd5a8c9f4b5065e531f799",
+    "url": "/static/media/error.b5ae6b7f.svg"
+  },
+  {
+    "revision": "dd75a11d52191d7141ce4668bba1700c",
+    "url": "/static/media/expand.dd75a11d.svg"
+  },
+  {
+    "revision": "bed8b2a191f5dab3cb5bb57c9ebe3cb6",
+    "url": "/static/media/freepik.bed8b2a1.svg"
+  },
+  {
+    "revision": "aa6cdd258d0654ae1491fa2d8c816b85",
+    "url": "/static/media/home.aa6cdd25.svg"
+  },
+  {
+    "revision": "38f5d4134f1a707e3a98b8efcf0f2a8f",
+    "url": "/static/media/info.38f5d413.svg"
+  },
+  {
+    "revision": "a0c8c731bbfe2cdc758f830eb7d1d72f",
+    "url": "/static/media/left.a0c8c731.svg"
+  },
+  {
+    "revision": "7cac421bb86a320d22885645d4105ef3",
+    "url": "/static/media/login.7cac421b.svg"
+  },
+  {
+    "revision": "bc654c6a669f537cf4ff31b501896dc0",
+    "url": "/static/media/logout.bc654c6a.svg"
+  },
+  {
+    "revision": "cb1314e1814804407e699d604aff1c21",
+    "url": "/static/media/material.cb1314e1.svg"
+  },
+  {
+    "revision": "f4f141470bf0b7b60e17fe085151579d",
+    "url": "/static/media/menu.f4f14147.svg"
+  },
+  {
+    "revision": "3590d3fcc022b601c17533d3fec19ad9",
+    "url": "/static/media/minimize.3590d3fc.svg"
+  },
+  {
+    "revision": "3aa5e8e6a32ed677345f2b0e96d52b80",
+    "url": "/static/media/minus.3aa5e8e6.svg"
+  },
+  {
+    "revision": "12c6f3c127d29e87b7cb5230133c2b04",
+    "url": "/static/media/moodboard.12c6f3c1.svg"
+  },
+  {
+    "revision": "a5abb4b6050580181089a6eb129497fa",
+    "url": "/static/media/plus.a5abb4b6.svg"
+  },
+  {
+    "revision": "d41e8f27ca7e35738a5f16508a36f809",
+    "url": "/static/media/project.d41e8f27.svg"
+  },
+  {
+    "revision": "5e9ee4e45652bc9f4e727b137dc8e220",
+    "url": "/static/media/right.5e9ee4e4.svg"
+  },
+  {
+    "revision": "c7c88e8e95d8c7a5f7c82a9d5b17c5cb",
+    "url": "/static/media/sad.c7c88e8e.svg"
+  },
+  {
+    "revision": "a831824360374d62ca2fc377f2adcc53",
+    "url": "/static/media/save.a8318243.svg"
+  },
+  {
+    "revision": "cff5560ce4e7a5cb1229b1846285d47b",
+    "url": "/static/media/settings.cff5560c.svg"
+  },
+  {
+    "revision": "c0ea5e58600445c9a1131c4d7a4ef577",
+    "url": "/static/media/signup.c0ea5e58.svg"
+  },
+  {
+    "revision": "37493bd707c66f0841a5fd4ef77aaea0",
+    "url": "/static/media/template.37493bd7.svg"
+  },
+  {
+    "revision": "591001d473cbcd8b3890611da75c69fa",
+    "url": "/static/media/up.591001d4.svg"
+  }
+]);
